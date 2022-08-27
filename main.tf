b473ef79f64a768ad0ff8a08e6d2f5ff4092a862
root <root@86ed3e3c5b1c.mylabserver.com>
2022-08-27 15:56:26 +0000

    .

--- main.tf
+++ main.tf
@@ -77,6 +77,37 @@ resource "aws_security_group" "test-securitygroup" {
     protocol         = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+  ingress {
+    description      = "80 from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "443 from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "icmp from anywhere"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 resource "aws_security_group" "test-securitygroup-elb" {

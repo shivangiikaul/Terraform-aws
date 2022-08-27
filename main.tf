2abb3b3c03f95bea8685bcc06ce8b1f9d876c898
root <root@86ed3e3c5b1c.mylabserver.com>
2022-08-27 18:22:16 +0000

    .

--- main.tf
+++ main.tf
@@ -122,6 +122,11 @@ resource "aws_security_group" "test-securitygroup-elb" {
     protocol         = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+ egress {
+    from_port        = 0
+    to_port          = -1
+    protocol         = "tcp"
+    security_group_id = aws_security_group.test-securitygroup.id
 }
 
 #resource "aws_security_group_rule" "test-rule" {

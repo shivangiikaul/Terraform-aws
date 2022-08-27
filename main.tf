d23e11d2846abd0bf93eb7a03e34a4b6a80968ae
root <root@86ed3e3c5b1c.mylabserver.com>
2022-08-27 14:35:56 +0000

    .

--- main.tf
+++ main.tf
@@ -112,10 +112,10 @@ resource "aws_security_group" "test-securitygroup-elb" {
   #  type = "web-server"
   #}
 #}
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  security_group_id    = "${aws_security_group.test-securitygroup.id}"
-  network_interface_id = "${aws_instance.test-server.primary_network_interface_id}"
-}
+#resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  #security_group_id    = "${aws_security_group.test-securitygroup.id}"
+  #network_interface_id = "${aws_launch_configuration.test-launchconfig.primary_network_interface_id}"
+#}
 
 resource "aws_key_pair" "test-keypair" {
   key_name   = "aws-key"

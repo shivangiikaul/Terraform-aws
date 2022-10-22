aacfb1d32b83546d341841bf66d37edf7ee69a45
shivangiikaul <52159574+shivangiikaul@users.noreply.github.com>
2022-10-22 16:07:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "test-db"
+}
 
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "test"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  default     = "test@1234"
+}

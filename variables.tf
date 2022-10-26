efc488d82d93846f7df2159e444cedf961d8733c
root <shivangikaul1997@gmail.com>
2022-10-26 11:23:38 +0000

    .

--- variables.tf
+++ variables.tf
@@ -2,16 +2,19 @@ variable "db_name" {
   description = "Name of the database"
   type        = string
   default     = "testdb"
+  sensitive   = true 
 }
 
 variable "db_username" {
   description = "Database username"
   type        = string
   default     = "test"
+   sensitive   = true
 }
 
 variable "db_password" {
   description = "Database password"
   type        = string
   default     = "test1234"
+   sensitive   = true
 }

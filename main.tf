3efae5a357922bbaf3bded6f5425210d755c8625
shivangiikaul <52159574+shivangiikaul@users.noreply.github.com>
2022-11-03 18:26:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,13 @@ module "networking" {
 module "ec2" {
   source     = "./modules/ec2"
 #  vpc        = module.networking.vpc
+  
 }
 
 module "RDS" {
   source    = "./modules/RDS"
-#  depends_on = [module.networking]
-#  db-subnet =  module.networking.test-subnet
+  vpc_security_group_ids = [module.SECURITY-GROUPS.test-db-sg-id]
+  subnet_ids = ["${module.networking.test-subnet}", "${module.networking.test-subnet2}"]
 }
 
 module "IAM" {
